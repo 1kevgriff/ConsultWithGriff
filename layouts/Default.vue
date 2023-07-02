6ef738aea89fb19ac6339dc111cdc834c500d235
Kevin Griffin <kevin@kevgriffin.com>
2023-07-02 15:58:15 +0000

    msg

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -8,6 +8,7 @@
       <slot />
     </div>
 
+    <Footer />
   </div>
 </template>
 <script lang="ts" setup>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,23 @@
 <template>
-    <div>
-  
+    <div class="container w-3/4 mx-auto flex">
+        <div class="w-1/2">
+            <p>picture. here</p>
+        </div>
+        <div class="w-1/2">
+            <h2>Hey y'all.  I'm Kevin.</h2>
+            <h3>I help businesses reach their potential through optimized technology processes.</h3>
+        </div>
+    </div>
+    <div class="container w-3/4 mx-auto">
+        <h3>Check out my latest article</h3>
+    </div>
+    <div class="container w-3/4 mx-auto flex">
+        <div class="w-1/2">
+           <p>Let's chat... blah blah</p>
+        </div>
+        <div class="w-1/2">
+           <p>another picture?</p>
+        </div>
     </div>
   </template>
   <script lang="ts" setup>
