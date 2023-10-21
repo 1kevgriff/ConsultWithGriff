2559abdd5b32ebf8a5539adbd94fb8a5f3711480
Kevin Griffin <kevin@kevgriffin.com>
2023-10-20 21:17:07 -0400

    wip

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary': '#6C7A89', // Slate Gray
+				'secondary': '#34495E', // Navy Blue
+				'accent-1': '#FDFEFE', // Soft White
+				'accent-2': '#B4D8E7', // Light Blue
+				'highlight': '#1A535C', // Deep Teal
+			}
+		},
 	},
 	plugins: [],
 }
