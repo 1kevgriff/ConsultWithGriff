f8a6273186a7184082a712a973db954214f9e0d5
Kevin Griffin <kevin@kevgriffin.com>
2023-11-24 03:22:54 +0000

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,30 @@ import sitemap from '@astrojs/sitemap';
 import vue from "@astrojs/vue";
 
 import tailwind from "@astrojs/tailwind";
+import Unfonts from 'unplugin-fonts/astro'
 
 // https://astro.build/config
-import {remarkReadingTime} from "./remark-reading-time.mjs";
+
+const unfontOptions = {
+  typekit: {
+    id: 'wou4pvn',
+    defer: true,
+    injectTo: 'head-prepend'
+  },
+};
 
 export default defineConfig({
   site: 'https://consultwithgriff.com',
   integrations: [mdx(), sitemap(), vue(), tailwind({
     applyBaseStyles: false
-  })],
+  }), Unfonts(unfontOptions)],
   markdown: {
-    remarkPlugins: [remarkReadingTime],
+    remarkPlugins: [],
     shikiConfig: {
       theme: "github-dark"
     }
   },
   redirects: {
-    'sitemap.xml' : 'sitemap-index.xml'
+    'sitemap.xml': 'sitemap-index.xml'
   }
 });
